ad5930c6ac0dc18de3f358f6db931a42e7048f30
gaowenbin <lotosv2010@163.com>
2022-02-21 21:54:19 +0800

    feat(test): 测试GIcon 和 GLoading组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,6 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <p>icon font</p>
-    <g-icon-more />
-    <g-icon-arrow />
-    <g-icon-cross />
-    <p>
-      <g-icon name="more" :style="{ width: '30px', height: '30px' }"></g-icon>
-    </p>
-    <p>
-      <g-icon
-        name="arrow"
-        :style="{ width: '30px', height: '30px', color: 'green' }"
-      />
-    </p>
-    <p>
-      <g-icon
-        name="cross"
-        :style="{ width: '30px', height: '30px', color: 'orange' }"
-      />
-    </p>
-    <p>
-      <g-icon
-        prefix="icon-"
-        name="auto"
-        :style="{ width: '30px', height: '30px', color: 'orange' }"
-      />
-    </p>
   </div>
 </template>
 
---
+++ src/views/Text.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="test">
+    <h1>g-lib test</h1>
+    <g-icon-more />
+    <g-icon-arrow />
+    <g-icon-cross />
+    <p>
+      <g-icon name="more" :style="{ width: '30px', height: '30px' }"></g-icon>
+    </p>
+    <p>
+      <g-icon
+        name="arrow"
+        :style="{ width: '30px', height: '30px', color: 'green' }"
+      />
+    </p>
+    <p>
+      <g-icon
+        name="cross"
+        :style="{ width: '30px', height: '30px', color: 'orange' }"
+      />
+    </p>
+    <p>
+      <g-icon
+        prefix="icon-"
+        name="auto"
+        :style="{ width: '30px', height: '30px', color: 'orange' }"
+      />
+    </p>
+    <p>
+      <g-loading height="100" width="100">
+        <div style="color: red; font-size: 16px">数据加载中....</div>
+      </g-loading>
+    </p>
+  </div>
+</template>
